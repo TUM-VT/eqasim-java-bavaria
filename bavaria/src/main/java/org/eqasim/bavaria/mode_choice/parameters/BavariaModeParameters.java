ae6b15fac30897b914c0fd9e1e1915005091d244
Johannes Müller <jo.hannes.mueller@tum.de>
2025-09-15 16:20:41 +0200

    Additional changes to mode choice Bavaria

--- bavaria/src/main/java/org/eqasim/bavaria/mode_choice/parameters/BavariaModeParameters.java
+++ bavaria/src/main/java/org/eqasim/bavaria/mode_choice/parameters/BavariaModeParameters.java
@@ -4,34 +4,55 @@ import org.eqasim.core.simulation.mode_choice.parameters.ModeParameters;
 
 public class BavariaModeParameters extends ModeParameters {
 
+	public class BavariaWalkParameters {
+		public double isHighIncome;
+		public double hasDrivingPermit;
+		public double hasPtSubscription;
+		public double isMunichResident;
+	}
 
-	public class BavariaPtParameters {
-		public double betaDrivingPermit_u;
-		public double onlyBus_u;
+	public class BavariaBicycleParameters {
+		public double isHighIncome;
 	}
 
 	public class BavariaCarParameters {
 		public double isHighIncome;
-	}
-	
-	public class BavariaWalkParameters {
-		public double isHighIncome;
+		public double hasPtSubscription;
+		public double isWorkTrip;
+		public double isShoppingTrip;
 	}
 
 	public class BavariaCarPassengerParameters {
 		public double alpha_u;
 		public double betaInVehicleTravelTime_u_min;
 		public double betaDrivingPermit_u;
+		public double isHighIncome;
+		public double isWorkTrip;
+		public double isShoppingTrip;
 	}
 
-	public final BavariaCarPassengerParameters carPassenger = new BavariaCarPassengerParameters();
-
-	public final BavariaPtParameters bavariaPt = new BavariaPtParameters();
-
-	public final BavariaCarParameters bavariaCar = new BavariaCarParameters();
+	public class BavariaPtParameters {
+		public double betaDrivingPermit_u;
+		public double onlyBus_u;
+		public double isHighIncome;
+		public double waitingTimeHighIncome;
+		public double waitingTimeMunichResident;
+		public double waitingTimeSubscription;
+		public double isWorkTrip;
+		public double isShoppingTrip;
+	}
 
 	public final BavariaWalkParameters bavariaWalk = new BavariaWalkParameters();
 
+	public final BavariaBicycleParameters bavariaBicycle = new BavariaBicycleParameters();
+
+	public final BavariaCarParameters bavariaCar = new BavariaCarParameters();
+
+	public final BavariaCarPassengerParameters bavariaCarPassenger = new BavariaCarPassengerParameters();
+
+	public final BavariaPtParameters bavariaPt = new BavariaPtParameters();
+
+	
 	public double betaAccessTime_u_min;
 
 	public static BavariaModeParameters buildDefault() {
@@ -45,16 +66,40 @@ public class BavariaModeParameters extends ModeParameters {
 		parameters.lambdaCostEuclideanDistance = 0.0; //-0.257501;
 		parameters.referenceEuclideanDistance_km = 4.4;
 
+		
+
+		// Walk
+		parameters.walk.alpha_u = 1.8; // 1.685152;
+		parameters.walk.betaTravelTime_u_min = -0.162285;
+		parameters.bavariaWalk.isHighIncome = -0.0217;
+		parameters.bavariaWalk.hasDrivingPermit = -0.0319;
+		parameters.bavariaWalk.hasPtSubscription = 0.0175;
+		parameters.bavariaWalk.isMunichResident = 0.0106;
+
+
+		// Bicycle
+		parameters.bike.alpha_u = -0.5; // -2.927596;
+		parameters.bike.betaTravelTime_u_min = -0.093485;
+		parameters.bavariaBicycle.isHighIncome = -0.0255;
+
+		
 		// Car
 		parameters.car.alpha_u = 0.4; // -0.201465;
 		parameters.car.betaTravelTime_u_min = -0.042431;
 		parameters.bavariaCar.isHighIncome = -0.0225;
+		parameters.bavariaCar.hasPtSubscription = -0.0540;
+		parameters.bavariaCar.isWorkTrip = 0.0682;
+		parameters.bavariaCar.isShoppingTrip = 0.0682;
 
 		// Car passenger
-		parameters.carPassenger.alpha_u = -1.4; // -1.713201;
-		parameters.carPassenger.betaDrivingPermit_u = -0.835542;
-		parameters.carPassenger.betaInVehicleTravelTime_u_min = -0.069976;
+		parameters.bavariaCarPassenger.alpha_u = -1.4; // -1.713201;
+		parameters.bavariaCarPassenger.betaDrivingPermit_u = -0.835542;
+		parameters.bavariaCarPassenger.betaInVehicleTravelTime_u_min = -0.069976;
+		parameters.bavariaCarPassenger.isHighIncome = -0.0398;
+		parameters.bavariaCarPassenger.isWorkTrip = 0.0210;
+		parameters.bavariaCarPassenger.isShoppingTrip = 0.0508;
 
+		
 		// PT
 		parameters.pt.alpha_u = 0.0;
 		parameters.pt.betaLineSwitch_u = -0.417658;
@@ -63,15 +108,13 @@ public class BavariaModeParameters extends ModeParameters {
 
 		parameters.bavariaPt.betaDrivingPermit_u = -0.531426;
 		parameters.bavariaPt.onlyBus_u = -1.416309;
+		parameters.bavariaPt.isHighIncome = -0.0174;
+		parameters.bavariaPt.isWorkTrip = 0.0426;
+		parameters.bavariaPt.isShoppingTrip = 0.0441;
+		parameters.bavariaPt.waitingTimeHighIncome = -0.1530;
+		parameters.bavariaPt.waitingTimeMunichResident = 0.0991;
+		parameters.bavariaPt.waitingTimeSubscription = 0.4132;
 
-		// Bike
-		parameters.bike.alpha_u = -0.5; // -2.927596;
-		parameters.bike.betaTravelTime_u_min = -0.093485;
-
-		// Walk
-		parameters.walk.alpha_u = 1.8; // 1.685152;
-		parameters.walk.betaTravelTime_u_min = -0.162285;
-		parameters.bavariaWalk.isHighIncome = -0.0217;
 
 		return parameters;
 	}
